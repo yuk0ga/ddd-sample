765aa88f20934ac0330a05729a31721ea23c723b
古家優 <koga@tagbangers.co.jp>
2020-11-24 14:08:01 +1000

    created order

---
+++ src/main/java/com/springfest/dddsample/order/Order.java
@@ -0,0 +1,15 @@
+package com.springfest.dddsample.order;
+
+import org.jmolecules.ddd.types.AggregateRoot;
+
+/**
+ * @author KOGA, Yu
+ */
+public class Order implements AggregateRoot {
+
+    @Override
+    public Object getId() {
+        return null;
+    }
+
+}
